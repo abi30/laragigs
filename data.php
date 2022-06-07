8b89930170198f1e71691e1b1f053d94fd16bd45
Abdulla al Rakib <abdulla.rakib@work.local>
2022-06-07 16:39:37 +0200

    finished 1.10 min

---
+++ data.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    [
+        'title' => 'Laravel Senior Developer',
+        'tags' => 'laravel,javascript,typescript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email1@mail.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.',
+    ],
+    [
+        'title' => 'Laravel junior Developer',
+        'tags' => 'laravel,javascript,typescript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email1@mail.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.',
+    ],
+    [
+        'title' => 'Software Developer',
+        'tags' => 'laravel,javascript,typescript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email1@mail.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.',
+    ],
+    [
+        'title' => 'java Senior Developer',
+        'tags' => 'java,javascript,typescript',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email1@mail.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.
+    Tempor aute amet est ut Lorem aute incididunt.',
+    ],
+];
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(5)->create();
+        \App\Models\Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel,javascript,typescript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston,MA',
+        //     'email' => 'email1@mail.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Culpa est aliqua et nisi aliqua consectetur ad in consequat 
+        //     ut aliqua et aliquip. Aliqua enim non occaecat officia 
+        //     voluptate velit consequat nostrud incididunt in quis mollit aute 
+        //     Lorem. Incididunt sit culpa officia esse elit aliqua. Aliquip quis
+        //      adipisicing Lorem nulla duis anim est ea in enim nulla reprehenderit. 
+        //      Ad anim labore cillum irure ipsum sint fugiat deserunt 
+        //     duis in quis duis ullamco adipisicing. Eu qui in tempor magna velit amet elit magna officia.',
+
+        // ]);
+        // Listing::create([
+        //     'title' => 'java Senior Developer',
+        //     'tags' => 'Java,javascript,typescript',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York,NY',
+        //     'email' => 'emailstark@mail.com',
+        //     'website' => 'https://www.stark.com',
+        //     'description' => 'Culpa est aliqua et nisi aliqua consectetur ad in consequat 
+        //     ut aliqua et aliquip. Aliqua enim non occaecat officia 
+        //     voluptate velit consequat nostrud incididunt in quis mollit aute 
+        //     Lorem. Incididunt sit culpa officia esse elit aliqua. Aliquip quis
+        //      adipisicing Lorem nulla duis anim est ea in enim nulla reprehenderit. 
+        //      Ad anim labore cillum irure ipsum sint fugiat deserunt 
+        //     duis in quis duis ullamco adipisicing. Eu qui in tempor magna velit amet elit magna officia.',
+
+        // ]);
+    }
+}
