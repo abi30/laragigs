631d7322cdb3b65a39c147bb5dd8b7262e6fb7ae
abi30 <rakib0751@gmail.com>
2022-06-10 00:36:30 +0200

    hello from home pc

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -55,6 +55,7 @@ class ListingController extends Controller
             "logo" =>  'nullable',
             "description" => 'required'
         ]);
+        // waiting for the next 
         return redirect('/');
     }
 
